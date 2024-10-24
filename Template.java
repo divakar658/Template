10b84dc6199554854635f372f702957ff63761f0
allaboina divakar <132629933+divakar658@users.noreply.github.com>
2024-10-24 22:02:39 +0530

    hello raina

---
+++ Template.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    static void raina(int arr[],int n)
+    {
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextInt();
+		    }
+		    raina(arr,n);
+		}
+
+	}
+}
